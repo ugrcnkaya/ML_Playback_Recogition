b27513ad3e830ba3be573b9ba651d85fba92eeb6
Ugurcan Kaya <11018661@stud.hocschule-heidelberg.de>
2023-12-21 22:25:24 +0100

    make as a method

--- TM_211223/main.py
+++ TM_211223/main.py
@@ -1,27 +1,30 @@
 import cv2
 import numpy as np
 
-player_img=cv2.imread("player4.png")
-icon_img=cv2.imread("previous_video.png")
+def match_template(template_img_path, icon_img_path, threshold=0.8):
+    template_img = cv2.imread(template_img_path)
+    icon_img = cv2.imread(icon_img_path)
 
-result = cv2.matchTemplate(player_img, icon_img, cv2.TM_CCOEFF_NORMED)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    result = cv2.matchTemplate(template_img, icon_img, cv2.TM_CCOEFF_NORMED)
+    _, _, _, max_loc = cv2.minMaxLoc(result)
 
+    yloc, xloc = np.where(result >= threshold)
+    w = icon_img.shape[1]
+    h = icon_img.shape[0]
 
-threshold = 0.8
-yloc, xloc = np.where(result >= threshold)
-w = icon_img.shape[1]
-h = icon_img.shape[0]
+    max_loc = (max_loc[0] + w, max_loc[1] + h)
+    cv2.rectangle(template_img, max_loc, (max_loc[0] - w, max_loc[1] - h), (0, 255, 0), 2)
 
-max_loc = (max_loc[0] + w, max_loc[1] + h)
-cv2.rectangle(player_img, max_loc, (max_loc[0] - w, max_loc[1] - h), (0, 255, 0), 2)
+    cv2.imshow("Template", template_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow("player", player_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print("Max Matching Location (x, y):", max_loc[0], max_loc[1])
+    return max_loc[0], max_loc[1], template_img
 
-
+template_path = "player4.png"
+icon_path = "previous_video.png"
+x, y, result_img = match_template(template_path, icon_path)
+print("Max Matching Location (x, y):", x, y)
 
 
 
